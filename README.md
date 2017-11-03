3047780a24c7189e6bfe1bf774235562e63dfd91
Jeff Wu <WuTheFWasThat@gmail.com>
2017-11-03 00:02:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# YoutubeFocus
+
+Userscript which makes Youtube videos display in "mini-fullscreen" mode, which I call focus mode.
+It can be toggled easily with `m`.
+
+## Install
+
+If you have Tampermonkey/Greasemonkey installed, then just [click this link](https://github.com/WuTheFWasThat/YoutubeFocus/raw/master/youtube-focus.user.js)
+
+## Alternatives
+
+This is sort of similar to [Zren/ResizeYoutubePlayerToWindowSize](https://github.com/Zren/ResizeYoutubePlayerToWindowSize), but much fewer lines of code, and works better for me.
+I'm sure there are others.
+
+## Features
+
+- Remaps `m` to toggling focus mode (on by default, of course!)
+- Remaps `enter` to pause/resume, plus toggle focus mode based on that (good if you want focus mode IFF the video is playing, like I do)
+- Remaps `h` to rewind (instead of `j`)
+- Remaps `space` and `p` to pause/resume
+- Remaps `,` and `.` to speed changes (normally it's `<` and `>`, so you save a keystroke!)
+
+This may seem arbitrary to you since it was designed for my worfklow.
+Luckily my script is simple enough that perhaps you can tweak it!
+
+## Why?
+
+I made this for myself to solve a perhaps random set of problems, arising from my use of Vimium.
+
+- I wanted something like youtube full-screen, but more lightweight, and with other vimium unaffected (in actual full-screen, I can't use Vomnibar.activateTabSelection, for example).
+- Youtube uses `j` and `k` for rewinding and pause/resume.  This annoying since I'm used to these being scrolling.
+
+So here's my workflow:
+
+- I tell vimium to let the keys `hlmp><,.` pass through to the page
+- As a result, I end up with the more intuitive (to me) bindings:
+  - `,` and `.` change speed
+  - `h` rewinds and `l` fast-forwards
+  - `enter` to toggle watching
+  - `j` and `k` scroll (only useful outside of focus mode)
---
+++ youtube-focus.user.js
@@ -0,0 +1,132 @@
+// ==UserScript==
+// @name         Youtube-focus
+// @namespace    wuthefwasthat
+// @version      0.1
+// @description  Youtube focus
+// @match        *://www.youtube.com/watch*
+// @copyright    2017+, You
+// @grant        none
+// ==/UserScript==
+
+function addStyle(style_str, id) {
+  var el = document.createElement('style');
+  el.type = "text/css";
+  if (id) el.id = id;
+  if (el.styleSheet) { el.styleSheet.cssText = str; } // IE only
+  else { el.appendChild(document.createTextNode(style_str)); }
+  document.head.appendChild(el);
+}
+
+function isPlaying() {
+  var vid = document.querySelector("video");
+  return (!!(vid.currentTime > 0 && !vid.paused && !vid.ended && vid.readyState > 2));
+}
+
+const styleElemId = 'yt-custom-style';
+
+function focusModeOn() {
+  const styleEl = document.getElementById(styleElemId);
+  console.log('focus mode on');
+  if (styleEl) {
+    console.log('hmm, already has styleel?')
+    return;
+  }
+  addStyle(`
+    .html5-video-player {
+      position: fixed;
+      left: 0;
+      bottom: 0px;
+    }
+    .html5-video-container {
+      width: 100% !important;
+      height: 100% !important;
+    }
+    .html5-main-video {
+      width: 100% !important;
+      height: 100% !important;
+      left: 0px !important;
+    }
+    #masthead-positioner {
+      z-index: 0 !important;
+    }
+    #container {
+      display: none !important;
+    }
+    #main {
+      display: none;
+    }
+    #items {
+      display: none;
+    }
+  `, styleElemId);
+}
+
+function focusModeOff() {
+  const styleEl = document.getElementById(styleElemId);
+  console.log('focus mode off');
+  if (styleEl) {
+    styleEl.parentNode.removeChild(styleEl);
+  }
+}
+
+function toggleFocusMode() {
+  const styleEl = document.getElementById(styleElemId);
+  if (styleEl) {
+    focusModeOff();
+  } else {
+    focusModeOn();
+  }
+}
+
+// toggle once by default
+focusModeOn();
+
+function pauseOrResume() {
+  var vid = document.querySelector("video");
+  vid.paused ? vid.play() : vid.pause();
+}
+
+document.addEventListener("keydown", function(e) {
+  var keyCode = e.keyCode;
+  console.log('keyCode', keyCode);
+  if (keyCode === 13) { // enter
+    if (!isPlaying()) {
+      focusModeOn();
+    } else {
+      focusModeOff();
+    }
+    pauseOrResume();
+  } else if (keyCode === 80) { // p
+    pauseOrResume();
+  } else if (keyCode === 0 || keyCode === 32) { // space
+    pauseOrResume();
+  } else if (keyCode === 72) { // h
+    sendKey(74); // send a j
+  } else if (keyCode === 77) { // m
+    toggleFocusMode();
+  } else if (keyCode === 188) { // ,
+    if (e.shiftKey) { return; }
+    sendKey(keyCode, true); // send a <
+  } else if (keyCode === 190) { // .
+    if (e.shiftKey) { return; }
+    sendKey(keyCode, true); // send a >
+  }
+}, false);
+
+function sendKey(keyCode, shift) {
+  var keyboardEvent = document.createEvent("KeyboardEvent");
+  var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
+  keyboardEvent[initMethod](
+    "keydown", // event type : keydown, keyup, keypress
+     true, // bubbles
+     true, // cancelable
+     window, // viewArg: should be window
+     false, // ctrlKeyArg
+     false, // altKeyArg
+     !!shift, // shiftKeyArg
+     false, // metaKeyArg
+     keyCode, // keyCodeArg : unsigned long the virtual key code, else 0
+     0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
+  );
+  document.dispatchEvent(keyboardEvent);
+}
